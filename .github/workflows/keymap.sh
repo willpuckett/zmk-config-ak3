e9599fa3a087b86f70c75b7622a97c7c10b3710a
Will Puckett <willpuckett@gmail.com>
2024-11-20 13:37:04 -0800

    add studio unlock key, improve keymaps

---
+++ .github/workflows/keymap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROJECT=ak3
+LAYOUTS=('qwerty' 'engrammer' 'engrammer_shifted')
+ZMK_KEYMAP="config/boards/shields/${PROJECT}/${PROJECT}.keymap"
+DTS_LAYOUT=(--dts-layout "config/boards/shields/${PROJECT}/${PROJECT}-layouts.dtsi")
+
+# Iterate over array keys
+# https://devhints.io/bash#arrays
+for i in "${!LAYOUTS[@]}"; do
+    echo "Rendering Layout ${LAYOUTS[$i]}"
+    BASE=".images/ak3_${LAYOUTS[$i]}"
+    YML="$BASE.yml"
+    # note you have to have the quotes for KMD_LAYOUT expansion
+    KEYMAP_zmk_preamble="#define LAYOUT $i" keymap parse -z "$ZMK_KEYMAP" > "$YML" &&
+        keymap draw "${DTS_LAYOUT[@]}" "$YML" > "$BASE.svg"
+    [[ $? -ne 0 ]] && exit 1
+    rm "$YML"
+done
